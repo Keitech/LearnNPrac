bad2d60c79b960f0e452edceea80c267d270bfdc
Keith Tang <keithtang28@gmail.com>
2021-12-26 14:50:57 -0500

    inital commit

---
+++ leetcodeV1/Easy/twosum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        check = {}
+        
+        for i in range(len(nums)):
+            if target - nums[i] in check:
+                return [check[target-nums[i]] , i]
+            else:
+                check[nums[i]] = i
+
+run = Solution()
+
+run.twoSum([2,7,11,15], 9)
